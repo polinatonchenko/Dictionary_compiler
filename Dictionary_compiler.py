500774145ec667f0e48b151c66e6251be5d41362
Atepn <lockerf@mail.ru>
2022-06-28 10:14:02 -0400

    third commit

--- Dictionary_compiler.py
+++ Dictionary_compiler.py
@@ -1,14 +1,63 @@
-def dictionary_compiler(some_text:str, print_dictionary=False, exception_words=[]):
+import os
+import json
+
+def dictionary_compiler(some_text:str=None, txt_file:str=None):
     '''compiles dict (where the key is a word, the value is the number of repetitions) based on the some_text'''
-    fin_dict = {}
+    try: os.mkdir("texts")
+    except: FileExistsError
+    if some_text == None:
+        if txt_file != None:
+            with open(f"texts/{txt_file}", 'r') as file:
+                some_text = file.read().replace('\n', ' ')
+        else:
+            list_of_names = list_txt_files("texts")
+            if len(list_of_names) == 0:
+                print("No text")
+                return 
+            for n in list_of_names:
+                dictionary_compiler(txt_file=n)
+            return
+    if not is_accessible("dict.json"):
+        with open("dict.json", "w") as wf:
+            json.dump({}, wf)
+    with open("dict.json", "r") as rf:
+        fin_dict = json.load(rf)
     some_text = some_text.lower()
-    changed_text = "".join(c for c in some_text if c.isalpha() or c==" ")
+    changed_text = "".join(c for c in some_text if c.isalpha() or c==" ") # removes all symbols except letters from the text
     text_list = changed_text.split()
-    fin_dict = dict.fromkeys(text_list, 0)
+    step_dict = dict.fromkeys(text_list, 0)
     for w in text_list:
-        fin_dict[w] += 1 
-    print(fin_dict)
+        step_dict[w] += 1 
+    fin_dict = dict_combiner(fin_dict, step_dict)
+    with open("dict.json", "w") as wf:
+        json.dump(fin_dict, wf)
     return
+
+def list_txt_files(mydir = None):
+    '''creates a list of all file names in a directory with a .txt extension'''
+    list_of_names = []
+    for file in os.listdir(mydir):
+        if file.endswith(".txt"):
+            list_of_names.append(file)
+    return list_of_names
+
+def is_accessible(path, mode='r'):
+    '''Checking if a file or folder is from `path` available to work in the format provided by `mode`'''
+    try:
+        f = open(path, mode)
+        f.close()
+    except FileNotFoundError:
+        return False
+    return True
+
+def dict_combiner(dict1 , dict2):
+    '''creates a dictionary with keys from dict1 and dict2 and sum their values'''
+    for k in dict1:
+        if k in dict2:
+            dict1[k] += dict2[k]
+            dict2.pop(k)
+    dict1.update(dict2)
+    return dict1
 
 def replace_substrings(some_text:str, substrings_list:list=[], replace = ''):
     '''replace all substrings from substrings_list in some_text'''
@@ -16,5 +65,4 @@ def replace_substrings(some_text:str, substrings_list:list=[], replace = ''):
         some_text = some_text.replace(some_substring, replace)
     return some_text
 
-print('''In the deep, there are beasts so fell and terrible, that only they know what they are, for none who have met them have lived to tell of it... they are the Forgotten Beasts, born of the chaos from before the world's birth... they have waited, brooding in the dark places of the world... and now... by digging too deep... we have awakened them.''')
-dictionary_compiler('''In the deep, there are beasts so fell and terrible, that only they know what they are, for none who have met them have lived to tell of it... they are the Forgotten Beasts, born of the chaos from before the world's birth... they have waited, brooding in the dark places of the world... and now... by digging too deep... we have awakened them.''')
+dictionary_compiler()
