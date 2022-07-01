be8e78e6f5f2ea49db300e5fad0dea4d1dca9ac6
Atepn <lockerf@mail.ru>
2022-07-01 14:23:57 -0400

    second commit

--- DIctionary_compiler_class.py
+++ DIctionary_compiler_class.py
@@ -4,31 +4,62 @@ import json
 class DictOfWords:
 
     def __init__(self):
-        self.pathToFile = None
         self.dictionary = {}
+        # self.amountOfWords = 0
     
-    def dumpDict(self, pathToFile="dict.json"):
-        "biba boba"
+    def saveDict(self, pathToFile="dict.json"):
+        '''saves dictionary to json file in specified directory'''
         with open(pathToFile, "w") as wf:
             json.dump(self.dictionary, wf)
     
     def loadDict(self, pathToFile="dict.json"):
+        '''loads json dictionary from specified directory'''
         try:
             with open(pathToFile) as rf:
-                self.dictionary = json.load(rf)
+                stepDictionary = json.load(rf)
+                self.dictionary = self.__combineDict(self.dictionary, stepDictionary)
+                # self.amountOfWords = sum(self.dictionary.values())
         except: 
             print("No such file or directory")
 
     def eatText(self, someText:str):
+        '''collects statistics from semeText'''
         someText = someText.lower()
-        someText = "".join(c for c in someText if c.isalpha() or c==" ")
+        someText = "".join(c for c in someText if c.isalpha() or c==" " else ' ')
         wordsList = someText.split()
         stepDictionary = dict.fromkeys(wordsList, 0)
         for word in wordsList:
             stepDictionary[word] += 1
         self.dictionary = self.__combineDict(self.dictionary, stepDictionary)
+        # self.amountOfWords = sum(self.dictionary.values())
 
-    def __combineDict(dict1, dict2):
+    def eatFileTXT(self, pathToFile):
+        '''collects statistics from .txt file in the specified directory'''
+        try:
+            with open(pathToFile) as file:
+                someText = file.read().replace('\n', ' ')
+            self.eatText(someText)
+        except:
+            print("No such file or directory")
+
+    def eatDir(self, pathToDir=""):
+        '''collects statistics from all .txt files in the specified directory'''
+        if pathToDir != "":
+            pathToDir += "/"
+        listOfNames = self.__listFiles(pathToDir)
+        for name in listOfNames:
+            self.eatFileTXT(f"{pathToDir}{name}")
+    
+    def __listFiles(self, pathToDir):
+        '''creates a list of all file names in a directory with a .txt extension'''
+        listOfNames = []
+        for file in os.listdir(pathToDir):
+            if file.endswith(".txt"):
+                listOfNames.append(file)
+        return listOfNames
+
+    def __combineDict(self, dict1, dict2):
+        '''creates a dictionary with keys from dict1 and dict2 and sum their values'''
         for key in dict1:
             if key in dict2:
                 dict1[key] += dict2[key]
@@ -38,13 +69,12 @@ class DictOfWords:
 
     def __repr__(self):
         return repr(self.dictionary)
-
      
-
 def main():
     dict1 = DictOfWords()
-    dict1.loadDict()
-    print(dict1)
+    dict1.eatText("hello my dear, my pleasent DND - WORLD!")
+    print(dict1.dictionary)
+    # print(max(dict1.dictionary, key=dict1.dictionary.get))
 
 if __name__ == "__main__":
     main()
